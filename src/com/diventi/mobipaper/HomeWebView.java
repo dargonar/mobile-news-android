e044cb77b16dbce89767c8cb5754f42c799e6506
Marku <matias.romeo@gmail.com>
2014-07-18 01:35:06 -0300

    test pdf

--- src/com/diventi/mobipaper/HomeWebView.java
+++ src/com/diventi/mobipaper/HomeWebView.java
@@ -8,7 +8,9 @@ import com.diventi.mobipaper.ui.ActionsContentView;
 import android.annotation.SuppressLint;
 import android.content.Context;
 import android.content.Intent;
+import android.net.Uri;
 import android.util.AttributeSet;
+import android.util.Log;
 import android.view.MotionEvent;
 import android.view.View;
 import android.webkit.WebView;
@@ -51,6 +53,8 @@ public class HomeWebView extends BaseWebView
   
   public boolean shouldOverrideUrlLoading(WebView view, String url) 
   {
+    Log.e("HOMEWEBVIEW", url);
+    
     if( url.startsWith("noticia://") )
     {
       Intent articleIntent = new Intent(mContext, ArticleActivity.class);
@@ -67,6 +71,20 @@ public class HomeWebView extends BaseWebView
       return true;
     }
     
+    Log.e("HOMEWEBVIEW", "vengo....");
+    
+    if( url.startsWith("http") && url.indexOf(".pdf") != -1)
+    {
+      Log.e("HOMEWEBVIEW", "levanto....");
+      
+      Intent viewFileIntent = new Intent(Intent.ACTION_VIEW);
+      viewFileIntent.setDataAndType( Uri.parse(url), "application/pdf");
+      mContext.startActivity(viewFileIntent);
+      
+      Log.e("HOMEWEBVIEW", "salgo....");
+      return true;
+    }
+
     return false;
   }
   
